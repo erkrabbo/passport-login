705a5b048cec31dc1e199a59d1ef9f942c1e0e72
Luca Andaloro <luca.andaloro4@gmail.com>
2022-06-13 17:23:44 +0200

    ciao

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,41 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
 
         <!-- Styles -->
+        <style>
+            .button {
+                cursor: pointer;
+                font-weight: 500;
+                left: 3px;
+                line-height: inherit;
+                position: relative;
+                text-decoration: none;
+                text-align: center;
+                border-style: solid;
+                border-width: 1px;
+                border-radius: 3px;
+                -webkit-appearance: none;
+                -moz-appearance: none;
+                display: inline-block;
+            }
+
+            .button--small {
+                padding: 10px 20px;
+                font-size: 0.875rem;
+            }
+
+            .button--green {
+                outline: none;
+                background-color: #64d18a;
+                border-color: #64d18a;
+                color: white;
+                transition: all 200ms ease;
+            }
+
+            .button--green:hover {
+                background-color: #8bdda8;
+                color: white;
+            }
+        </style>
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
@@ -21,10 +56,33 @@
             <x-navbar/>
 
             <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2">
+                            <div id="dropin-container"></div>
+                            <button id="submit-button" class="button button--small button--green">Conferma</button>
+                        </div>
+                    </div>
+                </div>
+
                 <div id="app"></div>
 
                 <x-login-form/>
                 <x-register-form/>
+                <script>
+                    var button = document.querySelector('#submit-button');
+
+                    braintree.dropin.create({
+                        authorization: 'sandbox_gp967cyr_6gbmtt3qhmmnktb8',
+                        selector: '#dropin-container'
+                        }, function (err, instance) {
+                        button.addEventListener('click', function () {
+                            instance.requestPaymentMethod(function (err, payload) {
+                            // Submit payload.nonce to your server
+                            });
+                        })
+                    });
+                </script>
             </div>
         </div>
     </body>
